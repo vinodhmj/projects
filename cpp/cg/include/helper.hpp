8e679826850c9efd0d0e09b13bc12df903da0b69
vjayakri <vinodh.jayakrishnan@mathworks.in>
2018-07-11 16:02:05 +0530

    intersection algo

--- cpp/cg/include/helper.hpp
+++ cpp/cg/include/helper.hpp
@@ -4,26 +4,78 @@
 #include "elements.hpp"
 #include <fstream>
 
-namespace cg
-{
-  void readFromFile(const std::string& sfileName, std::vector<segment>& segments)
-  {
+namespace cg {
+void readFromFile(const std::string& sfileName, std::vector<segment>& segments) {
     std::ifstream fin(sfileName.c_str());
-    if (!fin)
-      throw std::runtime_error("Cannot find input file");
+    if (!fin) {
+        throw std::runtime_error("Cannot find input file");
+    }
 
     unsigned int nSize = 0;
     int x1(0), y1(0), x2(0), y2(0);
 
-    fin >>  nSize;
+    fin >> nSize;
     segments.reserve(nSize);
-    
-    for (unsigned int i=0; i < nSize; ++i) {
-      fin >> x1 >> y1 >> x2 >> y2;
-      segments.push_back(segment(x1,y1,x2,y2));
-    }//do for all segments in input file
-    
-  }//end of function readFromFile
-}// namespace cg
+
+    for (unsigned int i = 0; i < nSize; ++i) {
+        fin >> x1 >> y1 >> x2 >> y2;
+        segments.push_back(segment(x1, y1, x2, y2));
+    } // do for all segments in input file
+
+} // end of function readFromFile
+
+int direction(const point<int>& i, const point<int>& j, const point<int>& k) {
+    return (i.x - k.x) * (j.y - k.y) - (j.x - k.x) * (i.y - k.y);
+} // end of function direction
+
+template <typename T>
+bool isInBounds(const T& value, const T& low, const T& high) {
+    return (value >= low) && (value <= high);
+} // end of function isInBounds
+
+bool onSegment(const point<int>& i, const point<int>& j, const point<int>& k) {
+    bool xBound = isInBounds(k.x, std::min(i.x, j.x), std::max(i.x, j.x));
+    bool yBound = isInBounds(k.y, std::min(i.y, j.y), std::max(i.y, j.y));
+    if (xBound && yBound) {
+        return true;
+    } else {
+        return false;
+    }
+} // end of function onSegment
+
+bool segmentsIntersect(const segment& a, const segment& b) {
+    int d1 = direction(b.left, b.right, a.left);
+    int d2 = direction(b.left, b.right, a.right);
+    int d3 = direction(a.left, a.right, b.left);
+    int d4 = direction(a.left, a.right, b.right);
+
+    if (((d1 > 0 && d2 < 0) || (d1 < 0 && d2 > 0)) && ((d3 > 0 && d4 < 0) || (d3 < 0 && d4 > 0))) {
+        return true;
+    } else if (d1 == 0 && onSegment(b.left, b.right, a.left)) {
+        return true;
+    } else if (d2 == 0 && onSegment(b.left, b.right, a.right)) {
+        return true;
+    } else if (d3 == 0 && onSegment(a.left, a.right, b.left)) {
+        return true;
+    } else if (d4 == 0 && onSegment(a.left, a.right, b.right)) {
+        return true;
+    } else {
+        return false;
+    }
+} // end of function segmentIntersect
+
+} // namespace cg
+
+std::ostream &  operator<<(std::ostream &output, const cg::point<int>& pt) {
+    output << "(" << pt.x << "," << pt.y << ")";
+    return output;
+}
+
+std::ostream &  operator<<(std::ostream &output, const cg::segment& seg) {
+    output << seg.left << "\t" << seg.right << "\n";
+    return output;
+}
+
+
 
 #endif /* CG_HELPER_H */
--- cpp/cg/src/main.cpp
+++ cpp/cg/src/main.cpp
@@ -2,26 +2,25 @@
 #include "../include/elements.hpp"
 #include "../include/helper.hpp"
 
-int main(int argc, char *argv[])
-{
-  try
-  {
-  std::vector<cg::segment> segments;
-  std::string sFileName = "input.txt";
-  cg::readFromFile(sFileName, segments);
-  
-  // test case
-  // read from file
-  // find intersections
-  }
-  catch (std::exception& ex)
-  {
-    std::cout<<ex.what();
-  }
-  catch(...)
-  {
-    std::cout<<"elipsis";
-  }
-  return 0;
-}
+int main(int argc, char* argv[]) {
+    try {
+        std::vector<cg::segment> segments;
+        std::string sFileName = "input.txt";
+        cg::readFromFile(sFileName, segments);
+        bool ans = cg::segmentsIntersect(segments.at(0), segments.at(1));
+        std::cout << segments.at(0) << segments.at(1) << ": " << ans << std::endl;
+        ans = cg::segmentsIntersect(segments.at(2), segments.at(3));
+        std::cout << segments.at(0) << segments.at(1) << ": " << ans << std::endl;
+        
 
+        // find if colinear
+        // find if overlapping
+        // find intersections
+        std::cout << "The End" ;
+    } catch (std::exception& ex) {
+        std::cout << ex.what();
+    } catch (...) {
+        std::cout << "elipsis";
+    }
+    return 0;
+}
