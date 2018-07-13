7558ace773e42fa1fa94847bee2d045f3926ec68
vjayakri <vinodh.jayakrishnan@mathworks.in>
2018-07-13 18:04:54 +0530

    more segments

--- cpp/cg/src/main.cpp
+++ cpp/cg/src/main.cpp
@@ -10,13 +10,16 @@ int main(int argc, char* argv[]) {
         bool ans = cg::segmentsIntersect(segments.at(0), segments.at(1));
         std::cout << segments.at(0) << segments.at(1) << ": " << ans << std::endl;
         ans = cg::segmentsIntersect(segments.at(2), segments.at(3));
-        std::cout << segments.at(0) << segments.at(1) << ": " << ans << std::endl;
-        
+        std::cout << segments.at(2) << segments.at(3) << ": " << ans << std::endl;
+        ans = cg::segmentsIntersect(segments.at(4), segments.at(5));
+        std::cout << segments.at(4) << segments.at(5) << ": " << ans << std::endl;
+        ans = cg::segmentsIntersect(segments.at(6), segments.at(7));
+        std::cout << segments.at(6) << segments.at(7) << ": " << ans << std::endl;
 
         // find if colinear
         // find if overlapping
         // find intersections
-        std::cout << "The End" ;
+        std::cout << "The End";
     } catch (std::exception& ex) {
         std::cout << ex.what();
     } catch (...) {
