bc437dde3452ea7c95c050fe614c26e12b2a7d75
Vinodh Jayakrishnan <vj@mj.home>
2022-03-16 11:50:56 +0000

    protection coursework

--- python/protection.py
+++ python/protection.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import matplotlib
 from matplotlib import pyplot as plt
 
 def findTMS(t, I, psm, In):
@@ -19,14 +20,14 @@ def main():
     # Relay T
     # --------------------------------------------------
     print("RELAY T")
-    psm = 2
+    psm = 2.25
     In = 100
     tms = findTMS(0.51, 1270, psm, In)
     print("T: TMS", tms)
-    
-    faultCurrents = [500, 750, 1000, 1270, 1587, 2120, 3180, 4000];
+
+    faultCurrentsT = [350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 1000, 1100, 1270, 1587];
     timeT = []
-    for i in faultCurrents:
+    for i in faultCurrentsT:
         temp = findOperatingTime(tms, i, psm, In);
         timeT.append(temp);
         print("T: current,", i, ", time:", temp)
@@ -34,13 +35,16 @@ def main():
     # Relay S
     # --------------------------------------------------
     print("RELAY S")
-    psm = 3
+    psm = 4.25
     In = 100
+    t1 = 1.01
     tms = findTMS(1.01, 1587, psm, In)
     print("S: TMS", tms)
-    
+
+    faultCurrentsS = [500, 550, 600, 650, 700, 750, 800, 850, 900, 1000, 1100, 1270, 1587, 1700, 1800, 1900, 2000, 2120];
+
     timeS = []
-    for i in faultCurrents:
+    for i in faultCurrentsS:
         temp = findOperatingTime(tms, i, psm, In);
         timeS.append(temp);
         print("S: current,", i, ", time:", temp)
@@ -49,29 +53,58 @@ def main():
     # Relay R
     # --------------------------------------------------
     print("RELAY R")
-    psm = 3
+    psm = 6.5
     In = 100
     tms = findTMS(1.51, 2120, psm, In)
     print("R: TMS", tms)
-    
+
+    tms = 0.2
+    faultCurrentsR = [750, 800, 850, 900, 1000, 1100, 1270, 1587, 1700, 1800, 1900, 2000, 2120, 2250, 2400, 2600, 2800, 3180];
+
     timeR = []
-    for i in faultCurrents:
+    for i in faultCurrentsR:
         temp = findOperatingTime(tms, i, psm, In);
         timeR.append(temp);
         print("R: current,", i, ", time:", temp)
 
-  
-    plt.loglog(xI, yT, 'b', label="Fuse")
-    plt.loglog(faultCurrents, timeT, 'o-g', label="Relay T")
-    plt.loglog(faultCurrents, timeS, 'o-y', label="Relay S")
-    plt.loglog(faultCurrents, timeR, 'o-r', label="Relay R")
+    fig1, ax1 = plt.subplots()
+    ax1.plot(xI, yT, 'b', label="Fuse")
+    ax1.plot(faultCurrentsT, timeT, '-g', label="Relay T")
+    ax1.plot(faultCurrentsS, timeS, '-y', label="Relay S")
+    ax1.plot(faultCurrentsR, timeR, '-r', label="Relay R")
+#    ax1.axvline(x=1587, color='k', linestyle=':')
+
     plt.title('IDMT Relay Coordination')
     plt.xlabel('Current (A)')
     plt.ylabel('Time (s)')
+    ax1.set_xscale('log')
+    ax1.set_yscale('log')
+    ax1.set_xticks([100, 480, 1000, 1270, 1587, 2120, 3180])
+    ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
     plt.legend()
     plt.grid(visible=True, which='major', linewidth=2, linestyle='-') 
     plt.grid(visible=True, which='minor', linewidth=1, linestyle='--')
-    plt.show()
+    # plt.show()
+
+    ''' Save the PNG file '''
+    filename = "ED_Graphical_Solution.png"
+    plt.savefig(filename)
+
+    # plt.loglog(xI, yT, 'b', label="Fuse")
+    # plt.loglog(faultCurrentsT, timeT, '-g', label="Relay T")
+    # plt.loglog(faultCurrentsS, timeS, '-y', label="Relay S")
+    # plt.loglog(faultCurrentsR, timeR, '-r', label="Relay R")
+    # plt.axvline(x=1587, color='k', linestyle=':')
+
+    # plt.title('IDMT Relay Coordination')
+    # plt.xlabel('Current (A)')
+    # plt.ylabel('Time (s)')
+    # #plt.set_xscale('log')
+    # #plt.set_xticks([1, 10, 100, 1000, 1587, 2120, 3180])
+    # plt.legend()
+    # plt.grid(visible=True, which='major', linewidth=2, linestyle='-') 
+    # plt.grid(visible=True, which='minor', linewidth=1, linestyle='--')
+    # plt.show()
 
 if __name__ == '__main__':
         main()
