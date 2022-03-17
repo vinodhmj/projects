8bc53bf31d9ae3781fe4869b8c86489939929405
Vinodh Jayakrishnan <vj@mj.home>
2022-03-17 12:36:15 +0000

    protection coursework first draft

--- python/protection.py
+++ python/protection.py
@@ -11,70 +11,90 @@ def findOperatingTime(tms, I, psm, In):
     t = tms * (0.14/(math.pow((I/(psm*In)), 0.02) - 1));
     return round(t, 3);
 
-def main():
+def relayCoordination(tmsT, tmsS, tmsR):
+    print("Relay Coordination START")
+    print("--------------------------------------------------")
     # Fuse Characteristics
-    xI = [105, 130, 150, 210, 480]
-    yT = [10, 1, 0.4, 0.1, 0.01]
+    xI = [105, 130, 150, 210, 480, 3180]
+    yT = [10, 1, 0.4, 0.1, 0.01, 0.01]
 
-    # --------------------------------------------------
-    # Relay T
     # --------------------------------------------------
     print("RELAY T")
-    psm = 2.25
+    # --------------------------------------------------
+    psm = 3.25
+    print("T: PSM: ", psm)
     In = 100
+    print("T: Time to trip: ", 0.51)
     tms = findTMS(0.51, 1270, psm, In)
+    print("T: Calculated TMS", tms)
+    tms = tmsT
     print("T: TMS", tms)
 
-    faultCurrentsT = [350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 1000, 1100, 1270, 1587];
+    faultCurrentsT = [400, 480, 500, 550, 600, 650, 700, 750, 800, 850, 900, 1000, 1100, 1270, 1587];
     timeT = []
     for i in faultCurrentsT:
         temp = findOperatingTime(tms, i, psm, In);
         timeT.append(temp);
-        print("T: current,", i, ", time:", temp)
-    # --------------------------------------------------
-    # Relay S
+        if i in [480, 1270, 1587, 2120, 3180]:
+            print("T: current,", i, ", time:", temp)
+
+    tLocal = findOperatingTime(tms, 1587, psm, In)
+    print("T: Local fault action time: ", tLocal)
+    print("--------------------------------------------------")
     # --------------------------------------------------
     print("RELAY S")
-    psm = 4.25
+    # --------------------------------------------------
+    tTrip = tLocal + 0.5
+    psm = 5.5
+    print("S: PSM: ", psm)
     In = 100
-    t1 = 1.01
-    tms = findTMS(1.01, 1587, psm, In)
+    print("S: Time to trip: ", tTrip)
+    tms = findTMS(tTrip, 1587, psm, In)
+    print("S: Calculated TMS", tms)
+    tms = tmsS
     print("S: TMS", tms)
 
-    faultCurrentsS = [500, 550, 600, 650, 700, 750, 800, 850, 900, 1000, 1100, 1270, 1587, 1700, 1800, 1900, 2000, 2120];
+    faultCurrentsS = [600, 650, 700, 750, 800, 850, 900, 1000, 1100, 1270, 1587, 1700, 1800, 1900, 2000, 2120];
 
     timeS = []
     for i in faultCurrentsS:
         temp = findOperatingTime(tms, i, psm, In);
         timeS.append(temp);
-        print("S: current,", i, ", time:", temp)
+        if i in [480, 1270, 1587, 2120, 3180]:
+            print("S: current,", i, ", time:", temp)
 
-    # --------------------------------------------------
-    # Relay R
+    tLocal = findOperatingTime(tms, 2120, psm, In)
+    print("T: Local fault action time: ", tLocal)
+    print("--------------------------------------------------")
     # --------------------------------------------------
     print("RELAY R")
-    psm = 6.5
+    # --------------------------------------------------
+    tTrip = tLocal + 0.5
+    psm = 7.25
+    print("R: PSM: ", psm)
     In = 100
-    tms = findTMS(1.51, 2120, psm, In)
+    print("R: Time to trip: ", tTrip)
+    tms = findTMS(tTrip, 2120, psm, In)
+    print("R: Calculated TMS", tms)
+    tms = tmsR
     print("R: TMS", tms)
 
-    tms = 0.2
     faultCurrentsR = [750, 800, 850, 900, 1000, 1100, 1270, 1587, 1700, 1800, 1900, 2000, 2120, 2250, 2400, 2600, 2800, 3180];
 
     timeR = []
     for i in faultCurrentsR:
         temp = findOperatingTime(tms, i, psm, In);
         timeR.append(temp);
-        print("R: current,", i, ", time:", temp)
+        if i in [1270, 1587, 2120, 3180]:
+            print("R: current,", i, ", time:", temp)
 
     fig1, ax1 = plt.subplots()
     ax1.plot(xI, yT, 'b', label="Fuse")
     ax1.plot(faultCurrentsT, timeT, '-g', label="Relay T")
     ax1.plot(faultCurrentsS, timeS, '-y', label="Relay S")
     ax1.plot(faultCurrentsR, timeR, '-r', label="Relay R")
-#    ax1.axvline(x=1587, color='k', linestyle=':')
 
-    plt.title('IDMT Relay Coordination')
+    plt.title('IDMT Relay Coordination : Standard Inverse')
     plt.xlabel('Current (A)')
     plt.ylabel('Time (s)')
     ax1.set_xscale('log')
@@ -84,27 +104,22 @@ def main():
     plt.legend()
     plt.grid(visible=True, which='major', linewidth=2, linestyle='-') 
     plt.grid(visible=True, which='minor', linewidth=1, linestyle='--')
-    # plt.show()
+    plt.show()
 
     ''' Save the PNG file '''
-    filename = "ED_Graphical_Solution.png"
+    filename = "idmtstandard.png"
     plt.savefig(filename)
 
-    # plt.loglog(xI, yT, 'b', label="Fuse")
-    # plt.loglog(faultCurrentsT, timeT, '-g', label="Relay T")
-    # plt.loglog(faultCurrentsS, timeS, '-y', label="Relay S")
-    # plt.loglog(faultCurrentsR, timeR, '-r', label="Relay R")
-    # plt.axvline(x=1587, color='k', linestyle=':')
-
-    # plt.title('IDMT Relay Coordination')
-    # plt.xlabel('Current (A)')
-    # plt.ylabel('Time (s)')
-    # #plt.set_xscale('log')
-    # #plt.set_xticks([1, 10, 100, 1000, 1587, 2120, 3180])
-    # plt.legend()
-    # plt.grid(visible=True, which='major', linewidth=2, linestyle='-') 
-    # plt.grid(visible=True, which='minor', linewidth=1, linestyle='--')
-    # plt.show()
+def main():
+    tmsT = 0.1
+    tmsS = 0.15
+    tmsR = 0.2
+    #relayCoordination(tmsT, tmsS, tmsR);
+    
+    tmsT = 0.1
+    tmsS = 0.15
+    tmsR = 0.2
+    relayCoordination(tmsT, tmsS, tmsR);
 
 if __name__ == '__main__':
-        main()
+       main()
